51f432ce4cd08a67962254c490b3eeb826366ee3
Tom Stanley <tom.stanley@alliescomputing.com>
2019-11-01 10:56:27 +0000

    Story showing Postcoder working with a form

--- stories/AddressLookup.stories.js
+++ stories/AddressLookup.stories.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import Postcoder from "../src/index";
 
@@ -25,4 +25,64 @@ export const SearchByPostcodeNoPlaceHolder = () => (
 );
 SearchByPostcodeNoPlaceHolder.story = {
 	name: "1 - Search by postcode; no placeholder",
+};
+
+export const InAForm = () => {
+	const [storedAddress, setAddress] = useState({
+		addressline1: "",
+		addressline2: "",
+		postcode: "",
+		posttown: "",
+	});
+
+	const addAddressToState = address => {
+		setAddress(address);
+	};
+
+	return (
+		<form>
+			<legend>Enter your address</legend>
+			<label>Postcode search</label>
+			<Postcoder
+				{...defaultProps}
+				addressSelectedCallback={addAddressToState}
+			/>
+			<hr />
+			<label htmlFor="address_line_1">Address line 1</label>
+			<input
+				readOnly
+				type="text"
+				id="address_line_1"
+				value={storedAddress.addressline1}
+			/>
+			<br />
+			<label htmlFor="address_line_2">Address line 2</label>
+			<input
+				readOnly
+				type="text"
+				id="address_line_2"
+				value={storedAddress.addressline2}
+			/>
+			<br />
+			<label htmlFor="posttown">Post town</label>
+			<input
+				readOnly
+				type="text"
+				id="posttown"
+				value={storedAddress.posttown}
+			/>
+			<br />
+			<label htmlFor="postcode">Postcode</label>
+			<input
+				readOnly
+				type="text"
+				id="postcode"
+				value={storedAddress.postcode}
+			/>
+		</form>
+	);
+};
+
+InAForm.story = {
+	name: "3 - In situ within a form",
 };
